3194bbc5c0e14c0d5986ea0149074af0ae818c05
Livan Frometa <lfrometa@latamautos.com>
2018-09-08 17:49:01 -0500

    Completed all endpoints of the Project resource. Added TODO.postman_collection.json

--- src/main/scala/hello/ProjectController.scala
+++ src/main/scala/hello/ProjectController.scala
@@ -10,13 +10,6 @@ import scala.collection.JavaConverters._
 @RestController
 class ProjectController {
 
-  //TODO: Only for testing library
-  var projects = List(
-    ProjectDTO("1111-2222-3333-4444", "MyProject1", 1),
-    ProjectDTO("2222-2222-3333-4444", "MyProject2", 2),
-    ProjectDTO("3333-2222-3333-4444", "MyProject3", 3)
-  )
-
   @RequestMapping(value = Array("/projects"), method = Array(RequestMethod.GET))
   def getAll: java.util.List[ProjectDTO] = projects.asJava
 
@@ -28,22 +21,78 @@ class ProjectController {
 
   @PostMapping(value = Array("/projects"))
   def create(@RequestBody data: CreateProjectParameter): ProjectDTO = {
-    val newProject = ProjectDTO(UUID.randomUUID().toString, data.name, data.priority)
+    val newProject = ProjectDTO(UUID.randomUUID().toString, data.name, data.priority, List.empty[TaskDTO].asJava)
     projects = newProject :: projects
     newProject
   }
 
   @PutMapping(value = Array("/projects/{projectId}"))
-  def update(@PathVariable projectId: String, @RequestBody data: UpdateProjectParameter): ProjectDTO = {
-    val newProject = ProjectDTO(projectId, data.name, data.priority)
-    projects = newProject :: projects.filter(_.id != projectId)
-    newProject
+  def update(@PathVariable projectId: String, @RequestBody data: UpdateProjectParameter): Optional[ProjectDTO] = {
+    projects.find(_.id == projectId) match {
+      case Some(project) =>
+        val newProject = ProjectDTO(projectId, data.name, data.priority, project.tasks)
+        projects = newProject :: projects.filter(_.id != projectId)
+        Optional.of(newProject)
+      case _ => Optional.ofNullable(null)
+    }
   }
+
+  @DeleteMapping(value = Array("/projects/{projectId}"))
+  def delete(@PathVariable projectId: String): Optional[java.util.List[ProjectDTO]] = {
+    projects.find(_.id == projectId) match {
+      case Some(_) =>
+        projects = projects.filter(_.id != projectId)
+        Optional.of(projects.asJava)
+      case _ => Optional.ofNullable(null)
+    }
+  }
+
+  @PutMapping(value = Array("/projects/{projectId}/tasks"))
+  def addTask(@PathVariable projectId: String, @RequestBody data: CreateTaskParameter): Optional[ProjectDTO] = {
+    projects.find(_.id == projectId) match {
+      case Some(project) =>
+        val newTask = TaskDTO(UUID.randomUUID().toString, data.name, data.checked)
+        val newProject = project.withTask(newTask)
+        projects = newProject :: projects.filter(_.id != projectId)
+        Optional.of(newProject)
+      case _ => Optional.ofNullable(null)
+    }
+  }
+
+  //DATA
+  //TODO: Only for testing library
+  var tasksProject1 = List(
+    TaskDTO("9999-8888-7777-6666", "Task 1", checked = true),
+    TaskDTO("8888-8888-7777-6666", "Task 1", checked = false)
+  )
+
+  var projects = List(
+    ProjectDTO("1111-2222-3333-4444", "MyProject1", 1, List.empty[TaskDTO].asJava),
+    ProjectDTO("2222-2222-3333-4444", "MyProject2", 2, List.empty[TaskDTO].asJava),
+    ProjectDTO("3333-2222-3333-4444", "MyProject3", 3, List.empty[TaskDTO].asJava)
+  )
 
 }
 
-case class ProjectDTO(@BeanProperty var id: String, @BeanProperty var name: String, @BeanProperty var priority: Int)
+//DTO
+//TODO: Only for testing library
+case class ProjectDTO(@BeanProperty var id: String,
+                      @BeanProperty var name: String,
+                      @BeanProperty var priority: Int,
+                      @BeanProperty var tasks: java.util.List[TaskDTO]) {
 
+  def withTask(task: TaskDTO): ProjectDTO = {
+    this.tasks = (task :: tasks.asScala.toList).asJava
+    this
+  }
+}
+
+case class TaskDTO(@BeanProperty var id: String, @BeanProperty var name: String, @BeanProperty var checked: Boolean)
+
+//Parameter
+//TODO: Only for testing library
 case class CreateProjectParameter(name: String, priority: Int)
 
 case class UpdateProjectParameter(name: String, priority: Int)
+
+case class CreateTaskParameter(name: String, checked: Boolean)
--- src/main/scala/hello/TaskController.scala
+++ src/main/scala/hello/TaskController.scala
@@ -3,6 +3,6 @@ package hello
 import org.springframework.web.bind.annotation._
 
 @RestController
-class TodoController {
+class TaskController {
 
 }
